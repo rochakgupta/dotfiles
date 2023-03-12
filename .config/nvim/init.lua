26e0a369bf3ef0f8cceade2407a63b7c64b146e5
Rochak Gupta <rochak84@gmail.com>
2023-03-12 16:13:15 -0700

    .config/nvim: Add keymap to search keymaps via telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -286,6 +286,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
