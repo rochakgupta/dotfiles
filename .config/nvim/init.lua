8d296ef28187f773eb7ee93de73cadb5b319e0b2
Rochak Gupta <rochak84@gmail.com>
2023-03-18 17:56:11 -0700

    .config/nvim: Minor changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -523,6 +523,3 @@ require('custom.keymaps')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
