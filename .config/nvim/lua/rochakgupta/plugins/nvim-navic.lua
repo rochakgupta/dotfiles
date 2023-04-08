f0cf14a6388f22f79f1f42dd0a912940c04c69d2
Rochak Gupta <rochak84@gmail.com>
2023-04-08 16:34:13 -0700

    .config/nvim: Load nvim-navic lazily

--- .config/nvim/lua/rochakgupta/plugins/nvim-navic.lua
+++ .config/nvim/lua/rochakgupta/plugins/nvim-navic.lua
@@ -5,6 +5,7 @@ return {
     dependencies = {
       'neovim/nvim-lspconfig', -- Used and configured in nvim-lspconfig
     },
+    lazy = true,
     init = function()
       -- vim.b.navic_lazy_update_context = true
     end,
