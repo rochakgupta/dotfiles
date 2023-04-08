3f30b57baf64d7751d4753941675143d86f3a5bf
Rochak Gupta <rochak84@gmail.com>
2023-04-08 16:03:05 -0700

    .config/nvim: Add codi plugin

---
+++ .config/nvim/lua/rochakgupta/plugins/codi.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'metakirby5/codi.vim',
+    cmd = { 'Codi', 'CodiNew', 'CodiSelect' },
+    lazy = true,
+    init = function()
+      -- vim.cmd([[let g:codi#virtual_text_pos = "right_align"]])
+    end,
+  },
+}
