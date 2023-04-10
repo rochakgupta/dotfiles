a337c199377c01009834fc6b689ee863c34248f7
Rochak Gupta <rochak84@gmail.com>
2023-04-09 23:40:22 -0700

    .config/nvim: Add vim-easy-align plugin

---
+++ .config/nvim/lua/rochakgupta/plugins/vim-easy-align.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'junegunn/vim-easy-align',
+    init = function()
+      -- Start interactive EasyAlign in visual mode (e.g. vipga)
+      vim.keymap.set('x', 'ga', '<Plug>(EasyAlign)', { noremap = false })
+      -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
+      vim.keymap.set('n', 'ga', '<Plug>(EasyAlign)', { noremap = false })
+    end,
+  },
+}
