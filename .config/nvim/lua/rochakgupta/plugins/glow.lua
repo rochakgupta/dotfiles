ea0d9b2f4622d3c5a632f6dea2c83ed3b613a8a4
Rochak Gupta <rochak84@gmail.com>
2023-05-11 01:37:18 -0700

    .config/nvim: Add glow plugin

---
+++ .config/nvim/lua/rochakgupta/plugins/glow.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'ellisonleao/glow.nvim',
+    config = true,
+    cmd = 'Glow',
+    opts = {
+      border = require('rochakgupta.settings').border,
+      width = 200,
+      width_ratio = 0.8,
+      height_ratio = 0.8,
+    },
+  },
+}
