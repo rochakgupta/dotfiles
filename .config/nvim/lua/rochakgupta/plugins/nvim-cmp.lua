7c5bbdf4abafa13cdd88515a482224275dc7dfa2
Rochak Gupta <rochak84@gmail.com>
2023-05-12 22:25:58 -0700

    .config/nvim: Add cmp-buffer plugin

--- .config/nvim/lua/rochakgupta/plugins/nvim-cmp.lua
+++ .config/nvim/lua/rochakgupta/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
     'hrsh7th/nvim-cmp',
     dependencies = {
       'hrsh7th/cmp-nvim-lsp', -- Used and configured in nvim-lspconfig
+      'hrsh7th/cmp-buffer',
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
     },
