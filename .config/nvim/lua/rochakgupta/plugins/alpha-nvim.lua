1d30c97f1a5bf069fec431edebbc816c287d50e4
Rochak Gupta <rochak84@gmail.com>
2023-05-16 21:48:03 -0700

    .config/nvim: Update alpha plugin config

--- .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
+++ .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
@@ -7,9 +7,9 @@ return {
       local alpha = require('alpha')
       local dashboard = require('alpha.themes.dashboard')
       dashboard.section.buttons.val = {
-        dashboard.button('e', '  New file', '<cmd>ene <CR>'),
-        dashboard.button('c', '  Config', ':e ~/.config/nvim/init.lua <CR>'),
-        dashboard.button('q', '  Quit', ':qa <CR>'),
+        dashboard.button('i', '  New file', '<cmd>ene<CR>'),
+        dashboard.button('c', '  Config', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
+        dashboard.button('q', '  Quit', ':qa<CR>'),
       }
       alpha.setup(dashboard.config)
     end,
