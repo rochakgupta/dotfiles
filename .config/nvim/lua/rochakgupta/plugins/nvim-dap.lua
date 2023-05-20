736401b1e73c8fb6175c2f6b3e63dee57a7c448b
Rochak Gupta <rochak84@gmail.com>
2023-05-19 19:09:14 -0700

    .config/nvim: Use default icons for nvim-dap-ui

--- .config/nvim/lua/rochakgupta/plugins/nvim-dap.lua
+++ .config/nvim/lua/rochakgupta/plugins/nvim-dap.lua
@@ -49,24 +49,7 @@ return {
 
       -- Dap UI setup
       -- For more information, see |:help nvim-dap-ui|
-      dapui.setup({
-        -- Set icons to characters that are more likely to work in every terminal.
-        --    Feel free to remove or use ones that you like more! :)
-        --    Don't feel like these are good choices.
-        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-        controls = {
-          icons = {
-            pause = '⏸',
-            play = '▶',
-            step_into = '⏎',
-            step_over = '⏭',
-            step_out = '⏮',
-            step_back = 'b',
-            run_last = '▶▶',
-            terminate = '⏹',
-          },
-        },
-      })
+      dapui.setup({})
 
       dap.listeners.after.event_initialized['dapui_config'] = dapui.open
       dap.listeners.before.event_terminated['dapui_config'] = dapui.close
