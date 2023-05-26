2eba757489cadce49b3a99fda8106e6e8114125e
Rochak Gupta <rochak84@gmail.com>
2023-05-26 00:00:06 -0700

    .config/nvim: Update alpha dashboard header

--- .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
+++ .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
@@ -6,6 +6,16 @@ return {
     config = function()
       local alpha = require('alpha')
       local dashboard = require('alpha.themes.dashboard')
+      dashboard.section.header.val = {
+        '                                                     ',
+        '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+        '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+        '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+        '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+        '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+        '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+        '                                                     ',
+      }
       dashboard.section.buttons.val = {
         dashboard.button('i', '  New file', '<cmd>ene<CR>'),
         dashboard.button('c', '  Config', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
