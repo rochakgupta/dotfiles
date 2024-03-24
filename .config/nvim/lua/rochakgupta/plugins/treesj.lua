39b55bbcb7fa1c22010cdf662d7d775f4436b383
Rochak Gupta <rochak84@gmail.com>
2024-03-23 21:40:10 -0700

    .config/nvim: Add treesj plugin

---
+++ .config/nvim/lua/rochakgupta/plugins/treesj.lua
@@ -0,0 +1,12 @@
+return {
+  'Wansmer/treesj',
+  keys = { '<space>j' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesj').setup({
+      use_default_keymaps = false,
+      max_join_length = 1000,
+    })
+    vim.keymap.set('n', '<leader>j', require('treesj').toggle, { desc = 'Toggle [J]oin via treesj' })
+  end,
+}
