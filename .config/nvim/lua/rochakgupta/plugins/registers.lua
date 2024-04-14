6dc46d3b8bc328063da0dbd4429a82e1c1a537a5
Rochak Gupta <rochak84@gmail.com>
2024-04-14 00:05:06 -0700

    .config/nvim: Add registers plugin

---
+++ .config/nvim/lua/rochakgupta/plugins/registers.lua
@@ -0,0 +1,11 @@
+return {
+  'tversteeg/registers.nvim',
+  opts = {
+    trim_whitespace = false,
+    system_clipboard = false,
+    window = {
+      border = require('rochakgupta.settings').border,
+      transparency = 0,
+    },
+  },
+}
