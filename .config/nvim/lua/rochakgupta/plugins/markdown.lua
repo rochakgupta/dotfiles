f1658545d00c44c799d84a97e53297202db8e9c6
Rochak Gupta <rochak84@gmail.com>
2024-05-14 23:18:17 -0700

    .config/nvim: Add markdown plugin for better inline markdown rendering

---
+++ .config/nvim/lua/rochakgupta/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+  'MeanderingProgrammer/markdown.nvim',
+  ft = 'markdown',
+  name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('render-markdown').setup({})
+  end,
+}
