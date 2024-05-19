51b1f045e2efb09fdc1356298e714ba24417a601
Rochak Gupta <rochak84@gmail.com>
2024-05-18 23:23:08 -0700

    .config/nvim: Add keymaps to switch between windows

--- .config/nvim/lua/rochakgupta/config/keymaps.lua
+++ .config/nvim/lua/rochakgupta/config/keymaps.lua
@@ -10,6 +10,12 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 -- Clear highlight on search upon pressing CTRL-C in normal mode
 vim.keymap.set('n', '<C-c>', '<cmd>nohlsearch<CR>')
 
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 -- Quickfix list keymaps
 vim.keymap.set('n', ']q', ':cnext<CR>', { desc = 'Go to next item in quickfix list' })
 vim.keymap.set('n', '[q', ':cprevious<CR>', { desc = 'Go to previous item in quickfix list' })
