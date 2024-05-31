f3a681ac23755f00ccce410fc0c23d9124006c27
Rochak Gupta <rochak84@gmail.com>
2024-05-30 21:40:01 -0700

    .config/nvim: Add shortcut to wexterm config in alpha dashboard

--- .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
+++ .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
@@ -20,6 +20,7 @@ return {
         dashboard.button('i', '  New file', '<cmd>ene<CR>'),
         dashboard.button('c', '  Neovim Config', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
         dashboard.button('t', '  Tmux Config', ':e ~/.tmux.conf<CR>'),
+        dashboard.button('w', '  WezTerm Config', ':e ~/.config/wezterm/wezterm.lua<CR>'),
         dashboard.button('z', '  Zsh Config', ':e ~/.zshrc<CR>'),
         dashboard.button('q', '  Quit', ':qa<CR>'),
       }
