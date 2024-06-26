7a5d90a60be80f6df7e114f0a78c772cd0c81493
Rochak Gupta <rochak84@gmail.com>
2024-06-25 21:27:22 -0700

    .config/nvim: Minor changes

--- .config/nvim/lua/rochakgupta/plugins/firenvim.lua
+++ .config/nvim/lua/rochakgupta/plugins/firenvim.lua
@@ -7,7 +7,6 @@ return {
   -- Lazy load firenvim
   -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
   cond = not not vim.g.started_by_firenvim,
-  -- cond = not not vim.g.started_by_firenvim,
   build = function()
     require('lazy').load({
       plugins = 'firenvim',
