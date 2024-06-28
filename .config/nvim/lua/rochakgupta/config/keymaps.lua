225e979b6c465e96d34d370fdd63d98b59224792
Rochak Gupta <rochak84@gmail.com>
2024-06-27 23:30:31 -0700

    .config/nvim: Update keymap to open diagnostic in floating window

--- .config/nvim/lua/rochakgupta/config/keymaps.lua
+++ .config/nvim/lua/rochakgupta/config/keymaps.lua
@@ -54,4 +54,4 @@ vim.keymap.set('n', '*', '*zz', { desc = 'Go to next cursor match and center' })
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
