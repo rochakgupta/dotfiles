f7cd2e1a641ceb9ec563234af8bb1ed62fdb8e23
Rochak Gupta <rochak84@gmail.com>
2024-07-03 02:21:06 -0700

    .config/nvim: Add shortcuts to lazygit, starship and vifm configs in alpha dashboard

--- .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
+++ .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
@@ -18,10 +18,13 @@ return {
     }
     dashboard.section.buttons.val = {
       dashboard.button('i', '  New file', '<cmd>ene<CR>'),
-      dashboard.button('c', '  Neovim Config', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('l', '  Lazygit Config', ':cd ~/.config/lazygit<CR>:e ~/.config/lazygit/config.yml<CR>'),
+      dashboard.button('n', '  Neovim Config', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('s', '  Starship Config', ':e ~/.config/starship.toml<CR>'),
       dashboard.button('t', '  Tmux Config', ':e ~/.tmux.conf<CR>'),
       dashboard.button('v', '  Vim Config', ':e ~/.vimrc<CR>'),
-      dashboard.button('w', '  WezTerm Config', ':e ~/.config/wezterm/wezterm.lua<CR>'),
+      dashboard.button('f', '  Vifm Config', ':cd ~/.config/vifm<CR>:e ~/.config/vifm/vifmrc<CR>'),
+      dashboard.button('w', '  WezTerm Config', ':cd ~/.config/wezterm<CR>:e ~/.config/wezterm/wezterm.lua<CR>'),
       dashboard.button('z', '  Zsh Config', ':e ~/.zshrc<CR>'),
       dashboard.button('q', '  Quit', ':qa<CR>'),
     }
