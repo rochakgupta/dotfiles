3dfe42ad7e6a156c421d45fa0fbd68b66ae85916
Rochak Gupta <rochak84@gmail.com>
2024-07-25 02:37:30 -0700

    .config/nvim: Add shortcut to bat config in alpha dashboard

--- .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
+++ .config/nvim/lua/rochakgupta/plugins/alpha-nvim.lua
@@ -17,15 +17,16 @@ return {
       '                                                     ',
     }
     dashboard.section.buttons.val = {
-      dashboard.button('i', '  New file', '<cmd>ene<CR>'),
-      dashboard.button('l', '  Lazygit Config', ':cd ~/.config/lazygit<CR>:e ~/.config/lazygit/config.yml<CR>'),
-      dashboard.button('n', '  Neovim Config', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
-      dashboard.button('s', '  Starship Config', ':e ~/.config/starship.toml<CR>'),
-      dashboard.button('t', '  Tmux Config', ':e ~/.tmux.conf<CR>'),
-      dashboard.button('v', '  Vim Config', ':e ~/.vimrc<CR>'),
-      dashboard.button('f', '  Vifm Config', ':cd ~/.config/vifm<CR>:e ~/.config/vifm/vifmrc<CR>'),
-      dashboard.button('w', '  WezTerm Config', ':cd ~/.config/wezterm<CR>:e ~/.config/wezterm/wezterm.lua<CR>'),
-      dashboard.button('z', '  Zsh Config', ':e ~/.zshrc<CR>'),
+      dashboard.button('i', '  New File', '<cmd>ene<CR>'),
+      dashboard.button('b', '  Config: bat', ':cd ~/.config/bat<CR>:e ~/.config/bat/config<CR>'),
+      dashboard.button('l', '  Config: lazygit', ':cd ~/.config/lazygit<CR>:e ~/.config/lazygit/config.yml<CR>'),
+      dashboard.button('n', '  Config: neovim', ':cd ~/.config/nvim<CR>:e ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('s', '  Config: starship', ':e ~/.config/starship.toml<CR>'),
+      dashboard.button('t', '  Config: tmux', ':e ~/.tmux.conf<CR>'),
+      dashboard.button('v', '  Config: vim', ':e ~/.vimrc<CR>'),
+      dashboard.button('f', '  Config: vifm', ':cd ~/.config/vifm<CR>:e ~/.config/vifm/vifmrc<CR>'),
+      dashboard.button('w', '  Config: wezterm', ':cd ~/.config/wezterm<CR>:e ~/.config/wezterm/wezterm.lua<CR>'),
+      dashboard.button('z', '  Config: zsh', ':e ~/.zshrc<CR>'),
       dashboard.button('q', '  Quit', ':qa<CR>'),
     }
     alpha.setup(dashboard.config)
