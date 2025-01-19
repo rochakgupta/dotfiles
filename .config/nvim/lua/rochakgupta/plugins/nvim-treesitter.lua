62b759fe9ab813811273937d0ee7df0aab0a3e13
Rochak Gupta <rochak84@gmail.com>
2025-01-20 02:10:58 +0530

    nvim: Change keymaps to swap siblings via sibling-swap plugin

--- .config/nvim/lua/rochakgupta/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/rochakgupta/plugins/nvim-treesitter.lua
@@ -79,15 +79,6 @@ return {
             ['[]'] = '@class.outer',
           },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
       },
     })
   end,
--- .config/nvim/lua/rochakgupta/plugins/sibling-swap.lua
+++ .config/nvim/lua/rochakgupta/plugins/sibling-swap.lua
@@ -7,7 +7,7 @@ return {
     require('sibling-swap').setup({
       use_default_keymaps = false,
     })
-    vim.keymap.set('n', '<C-h>', require('sibling-swap').swap_with_left, { desc = 'SiblingSwap: Swap with left sibling' })
-    vim.keymap.set('n', '<C-l>', require('sibling-swap').swap_with_right, { desc = 'SiblingSwap: Swap with right sibling' })
+    vim.keymap.set('n', '<leader>A', require('sibling-swap').swap_with_left, { desc = 'SiblingSwap: Swap with left sibling' })
+    vim.keymap.set('n', '<leader>a', require('sibling-swap').swap_with_right, { desc = 'SiblingSwap: Swap with right sibling' })
   end,
 }
