e730b6b5e67d73e5f1d9172c026fbe610c870295
Rochak Gupta <rochak84@gmail.com>
2025-01-19 16:53:44 +0530

    nvim: Add keymap to toggle recursive split/join via treesj plugin

--- .config/nvim/lua/rochakgupta/plugins/treesj.lua
+++ .config/nvim/lua/rochakgupta/plugins/treesj.lua
@@ -1,6 +1,6 @@
 return {
   'Wansmer/treesj',
-  keys = { '<leader>j' },
+  keys = { '<leader>j', '<leader>J' },
   dependencies = {
     'nvim-treesitter/nvim-treesitter', -- Configured in nvim-treesitter.lua
   },
@@ -10,5 +10,8 @@ return {
       max_join_length = 1000,
     })
     vim.keymap.set('n', '<leader>j', require('treesj').toggle, { desc = 'Treesj: Toggle join' })
+    vim.keymap.set('n', '<leader>J', function()
+      require('treesj').toggle({ split = { recursive = true } })
+    end, { desc = 'Treesj: Toggle recursive join' })
   end,
 }
