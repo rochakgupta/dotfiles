f5e0dbd7ec8a37407c9d55db75735e2ae1d6352e
Rochak Gupta <rochak84@gmail.com>
2025-01-20 23:48:32 +0530

    nvim: Disable wrapping for mini.bracketed plugin keymaps

--- .config/nvim/lua/rochakgupta/plugins/mini.lua
+++ .config/nvim/lua/rochakgupta/plugins/mini.lua
@@ -4,7 +4,22 @@ return {
   config = function()
     require('mini.align').setup()
 
-    require('mini.bracketed').setup()
+    require('mini.bracketed').setup({
+      buffer = { options = { wrap = false } },
+      comment = { options = { wrap = false } },
+      conflict = { options = { wrap = false } },
+      diagnostic = { options = { wrap = false } },
+      file = { options = { wrap = false } },
+      indent = { options = { wrap = false } },
+      jump = { options = { wrap = false } },
+      location = { options = { wrap = false } },
+      oldfile = { options = { wrap = false } },
+      quickfix = { options = { wrap = false } },
+      treesitter = { options = { wrap = false } },
+      undo = { options = { wrap = false } },
+      window = { options = { wrap = false } },
+      yank = { options = { wrap = false } },
+    })
 
     require('mini.comment').setup()
 
