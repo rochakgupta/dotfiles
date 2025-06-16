475787f8bfd65e58f627b16273f6c5b767ab5ec6
Rochak Gupta <rochak84@gmail.com>
2025-06-16 19:57:45 +0530

    wezterm: Format using stylua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 
 local native_fullscreen = false
 
@@ -7,78 +7,78 @@ local config = wezterm.config_builder()
 config.max_fps = 120
 
 config.window_padding = {
-	left = "0",
-	right = "0",
-	top = "0",
-	bottom = "0",
+  left = '0',
+  right = '0',
+  top = '0',
+  bottom = '0',
 }
 
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
 if native_fullscreen then
-	config.native_macos_fullscreen_mode = true
+  config.native_macos_fullscreen_mode = true
 end
 
-wezterm.on("window-resized", function(window, _)
-	local window_dims = window:get_dimensions()
-	local overrides = window:get_config_overrides() or {}
-	-- Hide tab bar when there is a single tab only in native fullscreen because
-	-- doing so in non-native (wezterm's) fullscreen causes wezterm to get cut by the notch.
-	overrides.hide_tab_bar_if_only_one_tab = native_fullscreen and window_dims.is_full_screen
-	window:set_config_overrides(overrides)
+wezterm.on('window-resized', function(window, _)
+  local window_dims = window:get_dimensions()
+  local overrides = window:get_config_overrides() or {}
+  -- Hide tab bar when there is a single tab only in native fullscreen because
+  -- doing so in non-native (wezterm's) fullscreen causes wezterm to get cut by the notch.
+  overrides.hide_tab_bar_if_only_one_tab = native_fullscreen and window_dims.is_full_screen
+  window:set_config_overrides(overrides)
 end)
 
-config.audible_bell = "Disabled"
+config.audible_bell = 'Disabled'
 
 local font = ({
-	{ family = "FiraMono", size = 12, weight = "Regular" },
-	{ family = "JetbrainsMono", size = 12, weight = "DemiBold" },
-	{ family = "Mononoki", size = 12.5, weight = "DemiBold" },
-	{ family = "SauceCodePro", size = 12, weight = "DemiBold" },
-	{ family = "UbuntuMono", size = 14, weight = "Regular" },
+  { family = 'FiraMono', size = 12, weight = 'Regular' },
+  { family = 'JetbrainsMono', size = 12, weight = 'DemiBold' },
+  { family = 'Mononoki', size = 12.5, weight = 'DemiBold' },
+  { family = 'SauceCodePro', size = 12, weight = 'DemiBold' },
+  { family = 'UbuntuMono', size = 14, weight = 'Regular' },
 })[4]
 
 config.font_size = font.size
 config.font = wezterm.font({
-	family = font.family .. " Nerd Font Mono",
-	weight = font.weight,
+  family = font.family .. ' Nerd Font Mono',
+  weight = font.weight,
 })
 -- Disable ligatures
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
-config.color_scheme = "Tokyo Night"
+config.color_scheme = 'Tokyo Night'
 config.colors = {
-	background = "black",
+  background = 'black',
 }
 
 config.keys = {
-	-- Use HOME as current working directory for new tab
-	{
-		key = "t",
-		mods = "SUPER",
-		action = wezterm.action.SpawnCommandInNewTab({
-			cwd = wezterm.home_dir,
-		}),
-	},
-	-- Do not prompt for confirmation when closing current tab
-	{
-		key = "w",
-		mods = "CMD",
-		action = wezterm.action.CloseCurrentTab({ confirm = false }),
-	},
+  -- Use HOME as current working directory for new tab
+  {
+    key = 't',
+    mods = 'SUPER',
+    action = wezterm.action.SpawnCommandInNewTab({
+      cwd = wezterm.home_dir,
+    }),
+  },
+  -- Do not prompt for confirmation when closing current tab
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentTab({ confirm = false }),
+  },
 }
 -- CTRL + number to activate that tab
 for i = 1, 9 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "CTRL",
-		action = wezterm.action.ActivateTab(i - 1),
-	})
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
 end
 
 return config
