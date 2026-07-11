bccbbb89025f1c9c0635ae0eb842816589932f5f
Rochak Gupta <rochak84@gmail.com>
2026-07-11 17:35:25 +0530

    nvim: Update nvim-cmp and related plugins and use nvim-cmp instead of blink

--- .config/nvim/lua/rochakgupta/config/globals.lua
+++ .config/nvim/lua/rochakgupta/config/globals.lua
@@ -6,11 +6,11 @@ vim.g.rg_colorscheme = vim.g.rg_color
 vim.g.rg_transparent = false
 
 -- Plugins --
-vim.g.rg_not_started_by_firenvim = not vim.g.started_by_firenvim
+vim.g.rg_not_firenvim = not vim.g.started_by_firenvim
 
 -- Completion
-vim.g.rg_blink = vim.g.rg_not_started_by_firenvim and vim.version().minor >= 10
-vim.g.rg_nvim_cmp = vim.g.rg_not_started_by_firenvim and not vim.g.rg_blink
+vim.g.rg_blink = false
+vim.g.rg_nvim_cmp = vim.g.rg_not_firenvim and not vim.g.rg_blink
 
 -- Picker
 vim.g.rg_fzf_lua = false
@@ -25,8 +25,8 @@ vim.g.rg_fidget = true
 vim.g.rg_lsp_progress = false
 
 -- Indent line
-vim.g.rg_indent_blankline = vim.g.rg_not_started_by_firenvim
-vim.g.rg_snacks_indent = vim.g.rg_not_started_by_firenvim and not vim.g.rg_indent_blankline
+vim.g.rg_indent_blankline = vim.g.rg_not_firenvim
+vim.g.rg_snacks_indent = vim.g.rg_not_firenvim and not vim.g.rg_indent_blankline
 
 -- Illuminate
 vim.g.rg_illuminate = false
@@ -38,8 +38,8 @@ vim.g.rg_mpls = not vim.g.rg_markdown_preview
 
 -- Others
 vim.g.rg_gitlinker = true
-vim.g.rg_gitsigns = vim.g.rg_not_started_by_firenvim -- Buggy and not needed in firenvim, so disabling
-vim.g.rg_lualine = vim.g.rg_not_started_by_firenvim
+vim.g.rg_gitsigns = vim.g.rg_not_firenvim -- Buggy and not needed in firenvim, so disabling
+vim.g.rg_lualine = vim.g.rg_not_firenvim
 vim.g.rg_sleuth = false
 vim.g.rg_guess_indent = false
 vim.g.rg_yanky = true
